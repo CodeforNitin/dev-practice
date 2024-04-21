6f939fdd75b61a1d478534332156a0619989ff58
CodeforNitin <codeforni3@gmail.com>
2024-04-21 15:37:55 +0530

    todoServer - problem 1 solved

---
+++ week-2/Week-2-Assignments/02-nodejs/data.js
@@ -0,0 +1,7 @@
+const data = [
+    { "id": 1, "Buy groceries": "kanda,batata", "completed": false },
+    { "id": 2, "Walk the dog": "kutte ko ghumana", "completed": true },
+    { "id": 3, "Do laundry": "wheel sabun", "completed": false }
+];
+
+module.exports = data;
